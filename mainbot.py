0ada385e3582255e43d1ce8c967f8cc55656a5e6
Sanjanainos <73393083+Sanjanainos@users.noreply.github.com>
2020-10-24 19:24:04 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,8 +30,8 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('Ameen91741779')
-    api.update_profile(name=f'ameen|{follower_count(user)} Followers')
+    user = api.get_user('Sanjana0506')
+    api.update_profile(name=f'sanjana|{follower_count(user)} Followers')
     print(f'Updating Twitter Name : ameen|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
